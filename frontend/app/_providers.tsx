f19836e23274801c904e9a13f814cb5cc0084bd6
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-11-03 11:32:16 +0700

    feat: integrate React Query for data fetching and add Providers component

---
+++ frontend/app/_providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  // Create QueryClient per user session
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,50 +1,38 @@
 "use client";
 
 import { apiProxy } from "@/utils/api-proxy";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+type Person = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  postal_code: string;
+  salary: number;
+};
 
 export default function Home() {
-  const [people, setPeople] = useState<
-    {
-      id: number;
-      first_name: string;
-      last_name: string;
-    }[]
-  >([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(true);
-    try {
-      const requestData = async () => {
-        const people = await apiProxy.get("/api/people");
-        console.log("PROXY GET:", people);
-        setPeople(people);
-
-        // const echo = await apiProxy.post("/api/echo", {
-        //   message: "Hello, backend!",
-        // });
-        // console.log("PROXY POST:", echo);
-      };
-
-      requestData();
-    } catch (error) {
-      console.error("Error fetching data via proxy:", error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
+  const { data, error } = useQuery({
+    queryKey: ["people"],
+    queryFn: async () => {
+      const response = await apiProxy.get("/api/people");
+      return response as Person[];
+    },
+  });
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
   return (
     <div>
       <h1 className="text-3xl font-bold">
         Check console log and network devtools
       </h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
+      {data && (
         <div>
-          {people.map((person) => (
+          {data.map((person) => (
             <div key={person.id}>
               {person.first_name} {person.last_name}
             </div>
